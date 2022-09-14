4277211e0e0660c4af38866c3c1c8426aa54904a
probablyhamzah <probablyhamzah@gmail.com>
2022-09-14 22:09:50 +0530

    added window constructor

--- State.hpp
+++ State.hpp
@@ -1,4 +1,6 @@
 #include <SFML/System/Time.hpp>
+#include <SFML/Graphics.hpp>
+
 #pragma once
 
 class State
@@ -6,11 +8,13 @@ class State
 private:
 public:
     State() {};
-    State(const State& state) {};
+    State(sf::RenderWindow* window) {};
+    //State(const State& state) {};
     virtual ~State() {};
 
     virtual void enter(const State& state) = 0;
     virtual void exit() = 0;
+    virtual void processInput() = 0;
     virtual void update(sf::Time dt) = 0;
     virtual void render() = 0;
 };
